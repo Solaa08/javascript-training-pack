4650d98a1ec20440c3f425e160a221174dd8e1ee
Solaa08 <solarium_08@hotmail.fr>
2022-09-11 22:59:01 +0200

    debug

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-import Product, {calculateVat,calculateTotalVat} from '/Product.js'
+// import Product, {calculateVat,calculateTotalVat} from '/Product.js'
+
+class Product{
+  constructor(name,price){
+    this.name = name
+    this.price = price
+  }
+}
+
+function calculateVat(product,vat){
+  return product.price *(vat/100);
+}
+ 
+function calculateTotalVat(product,vat){
+  return product.price + calculateVat(product,vat);
+}
  
 const product = new Product('simple pc', 100);
+console.log(product)
  
 document.getElementById('ShowProduct').innerHTML=`
   name: ${product.name} <br/>
